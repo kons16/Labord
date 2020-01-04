1020a1f29191780116300ff0105fbf2535315e83
kons16 <ks1216r@gmail.com>
2020-01-04 14:31:06 +0900

    fix shell

--- import_env.sh
+++ import_env.sh
@@ -1,6 +1,6 @@
-echo -n "REACT_APP_API_URL=\"" > ./env.txt
-echo -n ${REACT_APP_API_URL} >> ./env.txt
-echo "\"" >> ./env.txt
-echo -n "REACT_APP_PASS=\"" >> ./env.txt
-echo -n ${REACT_APP_PASS} >> ./env.txt
-echo "\"" >> ./env.txt
+echo -n "REACT_APP_API_URL=\"" > ./.env
+echo -n ${REACT_APP_API_URL} >> ./.env
+echo "\"" >> ./.env
+echo -n "REACT_APP_PASS=\"" >> ./.env
+echo -n ${REACT_APP_PASS} >> ./.env
+echo "\"" >> ./.env
