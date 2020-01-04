00fafee5917d6c8b80897270c4d4ab50866c8c47
kons16 <ks1216r@gmail.com>
2020-01-04 16:11:39 +0900

    routerをhashに変更

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Top from './components/Top';
 import InputPost from './components/InputPost';
@@ -12,7 +12,7 @@ class App extends Component {
       window.onload();
     }
   }
-  
+
   render() {
     return (
       <div className="App">
